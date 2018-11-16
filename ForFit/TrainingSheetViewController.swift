276579ed1cb9eaa27f87e0acdbda4e4fd6b190e3
rubnsbarbosa <rubensbarbosa90@gmail.com>
2018-11-15 23:15:37 -0300

    CustomCellTraining

--- ForFit/TrainingSheetViewController.swift
+++ ForFit/TrainingSheetViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class TrainingSheetViewController: UIViewController {
+class TrainingSheetViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableViewTrainingSheet: UITableView!
-    
+   
     let training = [
-        ["cadeira-extensora", "flexao-plantar", "hack-machine", "agachamento", "leg-press", "mesa-flexora", "panturrilha-hack", "panturrilha-machine"],
-        []
+        ["3x12 Extensora", "3x12 FlexaoPlantar", "3x12 HackMachinee", "3x12 Agachamento", "3x12 LegPresss",
+         "3x12 MesaFlexora", "3x12 PanturrilhaHack", "3x12 PanturrilhaMachine"],
+        
+        ["3x12 LatPullDown", "3x12 PecFly", "3x12 PranchaAbdominal", "3x12 PuxadaAlta", "3x12 PuxadorFrente"],
+        
+        ["3x12 Extensora", "3x12 FlexaoPlantar", "3x12 HackMachinee", "3x12 Agachamento", "3x12 LegPresss",
+         "3x12 MesaFlexora", "3x12 PanturrilhaHack", "3x12 PanturrilhaMachine"],
+        
+        ["3x12 Extensora", "3x12 FlexaoPlantar", "3x12 HackMachinee", "3x12 Agachamento", "3x12 LegPresss",
+         "3x12 MesaFlexora", "3x12 PanturrilhaHack", "3x12 PanturrilhaMachine"],
+        
+        ["3x12 Extensora", "3x12 FlexaoPlantar", "3x12 HackMachinee", "3x12 Agachamento", "3x12 LegPresss",
+         "3x12 MesaFlexora", "3x12 PanturrilhaHack", "3x12 PanturrilhaMachine"],
+        
+        ["3x12 Extensora", "3x12 FlexaoPlantar", "3x12 HackMachinee", "3x12 Agachamento", "3x12 LegPresss",
+         "3x12 MesaFlexora", "3x12 PanturrilhaHack", "3x12 PanturrilhaMachine"],
     ]
     
     var page: Int!
@@ -34,8 +48,28 @@ class TrainingSheetViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return training[page].count
+        //return training.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableViewTrainingSheet.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomCellTraining
+        
+        let str = training[page][indexPath.row].components(separatedBy: " ")
+        
+        cell.customInit(machine: str[1], series: str[0])
+        
+//        cell.pictureTraining.image = UIImage(named: (training[indexPath.row] + ".jpg"))
+//        cell.machineLabel.text = training[indexPath.row]
+
+        return cell
+    }
 
     @IBAction func switchCustomTableViewAction(_ sender: UISegmentedControl) {
+        page = sender.selectedSegmentIndex
+        tableViewTrainingSheet.reloadData()
     }
     
 }
